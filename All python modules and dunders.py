c8669c8c25e26f7eb50b6fc5141b74c00c14df8a
BuzzerrdBaait <108156235+BuzzerrdBaait@users.noreply.github.com>
2023-04-27 23:43:51 -0500

    Just adding some notes
    Just all of the modules and dunders from python.org explained and hash tagged so you can put it in your code it you want.

---
+++ All python modules and dunders.py
@@ -0,0 +1,93 @@
+#__len__(): Used to return the length of an object.              #any() - Returns True if any item in an iterable object is true
+#__del__(): Used to delete an object.                            #ascii() - Returns a string containing a printable representation of an object
+#__add__(): Used to add two objects together.                    #bin() - Returns the binary version of a number
+#__sub__(): Used to subtract two objects.                        #bool() - Converts a value to a Boolean
+#__mul__(): Used to multiply two objects together.               #bytearray() - Returns an array of bytes
+#__truediv__(): Used to divide two objects.                      #bytes() - Returns a bytes object
+#__floordiv__(): Used to floor divide two objects.               #callable() - Checks if the object is callable
+#__mod__(): Used to take the modulus of two objects.             #chr() - Returns a character from the specified Unicode code
+#__pow__(): Used to calculate the power of two objects.                         #classmethod() - Converts a method into a class method
+#__and__(): Used to perform a logical AND operation between two objects.        #compile() - Returns the specified source as an object, ready to be executed
+#__or__(): Used to perform a logical OR operation between two objects.          #complex() - Returns a complex number
+#__xor__(): Used to perform a logical XOR operation between two objects.        #delattr() - Deletes the specified attribute
+#__eq__(): Used to check if two objects are equal.                              #dict() - Returns a dictionary
+#__ne__(): Used to check if two objects are not equal.                          #dir() - Returns a list of the specified object's properties and methods
+#__gt__(): Used to check if one object is greater than the other.               #divmod() - Returns the quotient and remainder of a division
+#__lt__(): Used to check if one object is less than the other.                  #enumerate() - Takes a collection and returns it as an enumerate object
+#__ge__(): Used to check if one object is greater than or equal to the other.   #eval() - Evaluates and executes an expression
+#__le__(): Used to check if one object is less than or equal to the other.      #exec() - Executes the specified code
+#__call__(): Used to call a function.                                           #filter() - Filters elements from a collection
+#__getattr__(): Used to return the value of an attribute.                       #float() - Returns a floating point number
+#__setattr__(): Used to set the value of an attribute.                          #format() - Formats a specified value
+#__getitem__(): Used to get an item from a container.                           #frozenset() - Returns a frozenset object
+#__setitem__(): Used to set an item in a container.                             #getattr() - Returns the value of the specified attribute
+#__delitem__(): Used to delete an item from a container.                        #globals() - Returns the current global symbol table
+#__iter__(): Used to get an iterator of an object.                              #hasattr() - Checks if an object has the specified attribute
+#__contains__(): Used to check if an item is present in a container.            #hash() - Returns the hash value of a specified object
+                                                                                #hex() - Converts a number to hexadecimal
+#help() - Invokes the built-in help system                                      #hex() - Converts a number to hexadecimal
+#id() - Returns the identity of an object                                       #input() - Reads and returns a line of input
+#int() - Returns an integer                                                     #isinstance() - Checks if an object is an instance of a specified class
+#issubclass() - Checks if a specified class is a subclass of another class      #iter() - Returns an iterator object
+#len() - Returns the length of an object                                        #list() - Returns a list
+#locals() - Returns an updated dictionary of the current local symbol table     #map() - Applies a function to all the items in an iterable object
+#max() - Returns the largest item in an iterable                                #memoryview() - Returns a memory view object
+#min() - Returns the smallest item in an iterable                 #object() - Returns a new object
+#oct() - Converts a number to octal                               #open() - Opens a file and returns a file object
+#ord() - Converts a character to its integer value                #pow() - Returns the value of x to the power of y
+#print() - Prints the specified message to the screen             #property() - Gets, sets, deletes a property
+#range() - Generates a sequence of numbers                        #repr() - Returns a printable representation of an object
+#reversed() - Returns a reversed iterator                         #round() - Rounds a number
+#set() - Returns a new set object                                 #setattr() - Sets an attribute of an object
+#slice() - Returns a slice object                                 #sorted() - Returns a sorted list
+#staticmethod() - Converts a method into a static method           #str() - Returns a string object
+#sum() - Sums the items of an iterable                            #super() - Returns an object that represents the parent class
+#tuple() - Returns a tuple                                        #type() - Returns the type of an object
+#vars() - Returns the __dict__ property of an object               #zip() - Returns an iterator, from two or more iterators
+
+#__len__(): Used to return the length of an object.              #any() - Returns True if any item in an iterable object is true
+#__del__(): Used to delete an object.                            #ascii() - Returns a string containing a printable representation of an object
+#__add__(): Used to add two objects together.                    #bin() - Returns the binary version of a number
+#__sub__(): Used to subtract two objects.                        #bool() - Converts a value to a Boolean
+#__mul__(): Used to multiply two objects together.               #bytearray() - Returns an array of bytes
+#__truediv__(): Used to divide two objects.                      #bytes() - Returns a bytes object
+#__floordiv__(): Used to floor divide two objects.               #callable() - Checks if the object is callable
+#__mod__(): Used to take the modulus of two objects.             #chr() - Returns a character from the specified Unicode code
+#__pow__(): Used to calculate the power of two objects.                         #classmethod() - Converts a method into a class method
+#__and__(): Used to perform a logical AND operation between two objects.        #compile() - Returns the specified source as an object, ready to be executed
+#__or__(): Used to perform a logical OR operation between two objects.          #complex() - Returns a complex number
+#__xor__(): Used to perform a logical XOR operation between two objects.        #delattr() - Deletes the specified attribute
+#__eq__(): Used to check if two objects are equal.                              #dict() - Returns a dictionary
+#__ne__(): Used to check if two objects are not equal.                          #dir() - Returns a list of the specified object's properties and methods
+#__gt__(): Used to check if one object is greater than the other.               #divmod() - Returns the quotient and remainder of a division
+#__lt__(): Used to check if one object is less than the other.                  #enumerate() - Takes a collection and returns it as an enumerate object
+#__ge__(): Used to check if one object is greater than or equal to the other.   #eval() - Evaluates and executes an expression
+#__le__(): Used to check if one object is less than or equal to the other.      #exec() - Executes the specified code
+#__call__(): Used to call a function.                                           #filter() - Filters elements from a collection
+#__getattr__(): Used to return the value of an attribute.                       #float() - Returns a floating point number
+#__setattr__(): Used to set the value of an attribute.                          #format() - Formats a specified value
+#__getitem__(): Used to get an item from a container.                           #frozenset() - Returns a frozenset object
+#__setitem__(): Used to set an item in a container.                             #getattr() - Returns the value of the specified attribute
+#__delitem__(): Used to delete an item from a container.                        #globals() - Returns the current global symbol table
+#__iter__(): Used to get an iterator of an object.                              #hasattr() - Checks if an object has the specified attribute
+#__contains__(): Used to check if an item is present in a container.            #hash() - Returns the hash value of a specified object
+                                                                                #hex() - Converts a number to hexadecimal
+#help() - Invokes the built-in help system                                      #hex() - Converts a number to hexadecimal
+#id() - Returns the identity of an object                                       #input() - Reads and returns a line of input
+#int() - Returns an integer                                                     #isinstance() - Checks if an object is an instance of a specified class
+#issubclass() - Checks if a specified class is a subclass of another class      #iter() - Returns an iterator object
+#len() - Returns the length of an object                                        #list() - Returns a list
+#locals() - Returns an updated dictionary of the current local symbol table     #map() - Applies a function to all the items in an iterable object
+#max() - Returns the largest item in an iterable                                #memoryview() - Returns a memory view object
+#min() - Returns the smallest item in an iterable                 #object() - Returns a new object
+#oct() - Converts a number to octal                               #open() - Opens a file and returns a file object
+#ord() - Converts a character to its integer value                #pow() - Returns the value of x to the power of y
+#print() - Prints the specified message to the screen             #property() - Gets, sets, deletes a property
+#range() - Generates a sequence of numbers                        #repr() - Returns a printable representation of an object
+#reversed() - Returns a reversed iterator                         #round() - Rounds a number
+#set() - Returns a new set object                                 #setattr() - Sets an attribute of an object
+#slice() - Returns a slice object                                 #sorted() - Returns a sorted list
+#staticmethod() - Converts a method into a static method           #str() - Returns a string object
+#sum() - Sums the items of an iterable                            #super() - Returns an object that represents the parent class
+#tuple() - Returns a tuple                                        #type() - Returns the type of an object
+#vars() - Returns the __dict__ property of an object               #zip() - Returns an iterator, from two or more iterators
---
+++ SQL_queries.py
@@ -0,0 +1,124 @@
+import mysql
+
+from mysql.connector.errors import Error
+
+
+
+
+class SQLDatabase:
+    
+   
+    def __init__(self, dbname, host, user, password):
+        self.dbname = dbname
+        self.host = host
+        self.user = user
+        self.password = password
+ 
+    def connect(self):
+        try:
+            self.database = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                passwd=self.password,
+                database=self.dbname
+            )
+            print("Connected to database successfully!")
+        except Error as e:
+            print("Failed to connect to the database: {}".format(e))
+ 
+    def insert(self, query):
+        cursor = self.database.cursor()
+        try:
+            cursor.execute(query)
+            self.database.commit()
+            print("Data inserted successfully!")
+        except Error as e:
+            print("Failed to insert data: {}".format(e))
+ 
+    def query(self, query):
+        cursor = self.database.cursor()
+        try:
+            cursor.execute(query)
+            records = cursor.fetchall()
+            print("Data queried successfully!")
+            return records, print(records)
+        except Error as e:
+            print("Failed to query data: {}".format(e))
+ 
+    def update(self, query):
+        cursor = self.database.cursor()
+        try:
+            cursor.execute(query)
+            self.database.commit()
+            print("Data updated successfully!")
+        except Error as e:
+            print("Failed to update data: {}".format(e))
+ 
+    def delete(self, query):
+        cursor = self.database.cursor()
+        try:
+            cursor.execute(query)
+            self.database.commit()
+            print("Data deleted successfully!")
+        except Error as e:
+            print("Failed to delete data: {}".format(e))
+ 
+    def disconnect(self):
+        if self.database.is_connected():
+            self.database.close()
+            print("MySQL connection is closed")
+
+db = SQLDatabase('DATABASE_NAME', 'LOCAL_HOST or IP ADDRESS', 'USER_NAME', 'PASSWORD')######################<<<<<<DATABASE INFOOO!!!!!
+
+
+Table= ("NEEEEEEEEEEWWWWW_TAAAAAAAAAABBBBBBBBBLEEEEEEEEEEE")
+column1=("Username")
+column2=("User id")
+column3=("comment")
+column4=("comment_id")
+search1=("A")
+search2=("Jo")
+search3=1
+search4=5
+searchoptionslist=['%','_']
+zero=searchoptionslist[0]
+one=searchoptionslist[1]
+p1=['NEWTABLE']
+
+
+db.connect()
+
+class call_queries:
+
+    def search_table(): 
+        query = "SELECT * FROM {}".format(Table)
+        print("This is searching {} and printing the results".format(Table))
+        return query, (db.query(query))
+
+    
+    def search_column_startswith(): 
+        query = "SELECT * FROM {} WHERE {} LIKE '{}{}{}'".format(Table,column1,one, search1, zero)
+        print("This is searching {} for {} and printing results in the {} column".format(Table, search1, column1))
+        return query, (db.query(query))
+    
+    def search_column_contains():             
+        query = "SELECT * FROM {} WHERE {} LIKE '%{}%'".format(Table,column1,search1)
+        print("This is searching {} for {} and printing results in the {} column".format(Table, search1, column1))
+        return query, (db.query(query))
+    
+    def search_column_not_contains(): 
+        query = "SELECT * FROM {} WHERE {} NOT LIKE '%{}%'".format(Table,column1,search1)
+        print("This is searching {} for anything not containing {} and printing results in the {} column".format(Table, search1, column4))
+        return query, (db.query(query))
+    
+                
+    
+    def create_table_if_not_exists():
+        tableinp=input("What will your table's name be?")
+        query = ("CREATE TABLE IF NOT EXISTS {} (column1 INT,column2 VARCHAR(255),column3 DATETIME);").format(tableinp)
+        print("This is creating the table {} if it does not exist".format(tableinp))
+        return query, (db.query(query))
+
+    
+
+call_queries.create_table_if_not_exists()
